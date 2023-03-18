4695294de5461040b90d0184ee1f2b2e58e20d3c
singhmoe007 <morinasing@outlook.com>
2023-03-18 10:44:23 -0300

    Updated the dataset and generated a random variable!

--- random.js
+++ random.js
@@ -1,7 +1,17 @@
-// this is a program that will generate a random  for the User
+// this is a program that will generate a random Hip-hop beat idea for the User
+
+console.log("Your Randomly generated beat idea is: ")
 
 const dataSet = {
-
+    genre: ["Boom-Bap", "Trap", "Lofi", "OldSchool", "Drill", "Grime", "R&B", "pop", "Afro-beats", "DanceHall"],
+    instruments: ["Piano", "Guitar", "Flute", "Violen", "Saxophone"],
+    vibe: ["Chill", "Hard", "Dark", "Melodic", "Rock" ],
+    base: ["808", "Synth"]
 };
 
+let randomGenre = dataSet.genre[Math.floor(Math.random() * dataSet.genre.length)];
+let randomInstrument = dataSet.instruments[Math.floor(Math.random() * dataSet.instruments.length)];
+let randomVibe = dataSet.vibe[Math.floor(Math.random() * dataSet.vibe.length)];
+let randomBase = dataSet.base[Math.floor(Math.random() * dataSet.base.length)];
 
+console.log(randomBase);
